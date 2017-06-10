2b0f8e74d07fa221a87a9ef7eeb3e24a199e6c3b
victor4l <victoridongesit@gmail.com>
2017-06-10 12:44:05 +0100

    [feat] added tasks to automate sass conversion and browser reload

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,7 +1,10 @@
 import gulp from 'gulp';
 import browSync from 'browser-sync';
+import gulpSass from 'gulp-sass';
 
 const browserSync = browSync.create();
+
+// Setup browser-sync server
 gulp.task('browser-sync', () => {
   browserSync.init({
     server: {
@@ -10,8 +13,20 @@ gulp.task('browser-sync', () => {
   });
 });
 
-gulp.task('reload', ['browser-sync'], () => {
-  gulp.watch(['template/*.html', 'template/css/*.css'], () => {
-    browserSync.reload();
-  });
+// Reload browser
+gulp.task('reload', () => {
+  browserSync.reload();
+});
+
+// Task to convert sass to css
+gulp.task('sass', () => {
+  console.log('Converting sass to css');
+  gulp.src('template/scss/*.scss')
+    .pipe(gulpSass().on('error', gulpSass.logError))
+    .pipe(gulp.dest('template/css'));
+});
+
+// Watch sass and html files for changes, and run conversion task
+gulp.task('sass:watch', ['browser-sync'], () => {
+  gulp.watch(['template/scss/*.scss', 'template/*.html'], ['sass', 'reload']);
 });
