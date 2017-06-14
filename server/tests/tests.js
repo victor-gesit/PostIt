1e059b3cd676d491fa95e2eda0f19546f9db7f7e
victor4l <victoridongesit@gmail.com>
2017-06-15 00:34:13 +0100

    [feat] added coveralls config file

---
+++ server/tests/tests.js
@@ -0,0 +1,23 @@
+describe('PostIt Tests', () => {
+  describe('Routes test', () => {
+    it('ensures user sign in is successful', () => {
+
+    });
+    it('ensures user signup is successful', () => {
+
+    });
+    it('ensures group creation is successfull', () => {
+
+    });
+    it('ensures posting messages to groups is successful', () => {
+
+    });
+    it('ensures getting all messages for a group is successful', () => {
+
+    });
+    it('ensures getting all the members of a group is successful', () => {
+
+    });
+  });
+
+});
