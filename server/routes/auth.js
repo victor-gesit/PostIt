a18c8561ecd9ddadfba87e57f68e3700256ffb4d
victor4l <victoridongesit@gmail.com>
2017-06-24 22:06:03 +0100

    rework auth to use controllers

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,22 +3,46 @@ import passport from 'passport';
 
 
 // Load passport configuration
-import passport from '../auth/passport';
+import '../auth/passport';
 
 const router = express.Router();
 router.use(passport.initialize());
 router.use(passport.session());
 
-router.post('/api/user/signin', (req, res, next) => {
-  passport.authenticate('local.signin', (err, user, info) => {
-    if(err) {
-      return next(err);
-    }
-    if(!user) { return res.send({ error: 'User not found' });}
-    return res.send(user);
-  })
-});
+// Middlware to handle server errors during authentication
+/**
+ * @returns null
+ * @param {Object} err Error object sent from server
+ * @param {*} req Request object
+ * @param {*} res Response Object
+ * @param {*} next Next Callback function
+ */
+function authError(err, req, res, next) {
+  const statusCode = err.status || 500;
+  return res.status(statusCode).json({ error: 'Server error' });
+}
+router.use([authError]);
 
-router.post('/api/user/signup', (req, res, next) => {
+router.post('/signin',
+  passport.authenticate('local.signin', { failWithError: true }),
+  (req, res, next) => {
+    // Successful signin
+    return res.json({ id: req.user.id, message: 'Successful Signin' });
+  }, (err, req, res, next) => {
+    // Failure during signin
+    return res.json({ error: err, message: 'Error during signin' });
+  }
+);
 
-});
+router.post('/signup',
+  passport.authenticate('local.signup', { failWithError: true }),
+  (req, res, next) => {
+    // Successful signup
+    return res.json( { id: req.user.id, message: 'Successfull Signup' });
+  }, (err, req, res, next) => {
+    // Failure during signup
+    return res.json({ error: err, message: 'Error in signup' });
+  }
+);
+
+export default router;
