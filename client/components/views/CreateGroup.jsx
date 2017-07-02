58efd64d5e5dd98afe34d7a667e6a2a901d8c214
victor4l <victoridongesit@gmail.com>
2017-07-02 04:48:46 +0100

    break large components into smaller components

--- client/components/views/CreateGroup.jsx
+++ client/components/views/CreateGroup.jsx
@@ -1,18 +1,52 @@
 import React from 'react';
+import 'jquery';
+
+
 
 export default class CreateGroup extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedMembers: [],
+      registeredMembers: [{
+        email: "adebalogun@yahoo.com",
+        name: "Ade Balogun",
+      },
+      {
+        email: "johnsmith@yahoo.com",
+        name: "John Smith",
+      },
+      {
+        email: "joyokafor@yahoo.com",
+        name: "Joy Okafor",
+      },
+      {
+        email: "johnkennedy@yahoo.com",
+        name: "John Keneddy",
+      }],
+      // Method to add a member to the list of selected members
+      addMember: (selected, memberEmail) => {
+        if(selected) {
+          this.state.selectedMembers.push(memberEmail);
+        } else {
+          const index = this.state.selectedMembers.indexOf(memberEmail);
+          this.state.selectedMembers.splice(index, 1);
+        }
+      }
+    }
+  }
   render() {
     return(
       <div>
         <Nav/>
-        <Body/>
+        <Body addMember={this.state.addMember} registeredMembers={this.state.registeredMembers}/>
         <Footer/>
       </div>
     );
   }
 }
 
-
+// Navigation Bar
 class Nav extends React.Component {
   render() {
     return(
@@ -33,15 +67,40 @@ class Nav extends React.Component {
   }
 }
 
+// Page Body
 class Body extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedMembers : this.props.selectedMembers,
+      switchTab: (button, tabName) => {
+        var i, tabcontent, tablinks;
+        tabcontent = document.getElementsByClassName("tabcontent");
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+            tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        this.refs[tabName].style.display = "block";
+        this.refs[button].className += " active";
+      },
+
+    }
+  }
+  componentDidMount() {
+    this.refs['defaultTab'].click();
+  }
   render() {
     return(
-<div>
+      <div id="body">
+      <div id="main">
         <div className="tab">
-          <button className="tablinks" id="defaultTab" onclick={switchTab(event, 'info')}>Group info</button>
-          <button className="tablinks" id="add-members" onclick={switchTab(event, 'members')}>Add members</button>
+          <button className="tablinks" id="defaultTab" ref="defaultTab" onClick={() => this.state.switchTab("defaultTab", 'info')}>Group info</button>
+          <button className="tablinks" id="add-members" ref="add-members" onClick={() => this.state.switchTab("add-members", 'members')}>Add members</button>
         </div>
-        <div id="info" className="tabcontent">
+        <div id="info" ref="info" className="tabcontent">
           <div className="row">
             <div className="col s10 m8 l6">
               <div className="group-details">
@@ -54,48 +113,63 @@ class Body extends React.Component {
                     <textarea id="groupDescription" type="text" className="materialize-textarea" placeholder="Description" name="group-desc" defaultValue={""} />
                   </div>
                 </form>
-                <button className="btn" onclick="document.getElementById('add-members').click()">Next &gt;&gt;</button>
+                <button className="btn" onClick={() => this.state.switchTab(event, 'members')}>Next &gt;&gt;</button>
               </div>
             </div>
           </div>
         </div>
-        <div id="members" className="tabcontent">
+        <div id="members" ref="members" className="tabcontent">
           <div className="row">
             <div className="col s10 m8 l6">
-              <form>
+              <div>
                 <ul className="collection with-header">
                   <li className="collection-header"><h4>Add members</h4></li>
-                  <li className="collection-item">
-                    <input id="cb1" type="checkbox" name="IDforAdeBalogun" />
-                    <label htmlFor="cb1">Ade Balogun <small> adebalogun@yahoo.com</small></label>
-                  </li>
-                  <li className="collection-item">
-                    <input id="cb2" type="checkbox" name="IDforAdeBalogun" />
-                    <label htmlFor="cb2">John Smith <small>johnsmith@yahoo.com</small></label>
-                  </li>
-                  <li className="collection-item">
-                    <input id="cb3" type="checkbox" name="IDforAdeBalogun" />
-                    <label htmlFor="cb3">Sani Danjuma <small>sanidanjuma@gmail.com</small></label>
-                  </li>
-                  <li className="collection-item">
-                    <input id="cb4" type="checkbox" name="IDforAdeBalogun" />
-                    <label htmlFor="cb4">Joy Okafor <small>joyokafor@yahoo.com</small></label>
-                  </li>
+                  {
+                    this.props.registeredMembers.map((member, index) => 
+                      <RegisteredMember addMember={this.props.addMember} key={index} data={member}/>
+                    )
+                  }
                 </ul>
-              </form>
+              </div>
               <div>
-                <button className="btn" onclick="document.getElementById('defaultTab').click()">&lt;&lt; Group info</button>
+                <button className="btn" onClick={() => this.state.switchTab(event, 'info')}>&lt;&lt; Group info</button>
                 <button className="btn">Create group</button>
               </div>
             </div>
           </div>
         </div>
       </div>
+      </div>
     );
   }
 }
 
-
+// Component to contain a member loaded from the database
+class RegisteredMember extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: false,
+      // Method to decide if a member is to be added or not
+      set: (state, email) => {
+        this.state.selected = !this.state.selected;
+        this.props.addMember(this.state.selected, email);
+      }
+    }
+  }
+  render() {
+    return (
+      <li className="collection-item">
+        <input id={this.props.data.email} 
+        type="checkbox"
+        onClick={() => this.state.set(event, this.props.data.email)}
+        ref={this.props.data.email} />
+        <label className="brown-text" htmlFor={ this.props.data.email }>{this.props.data.name} <small className="red-text">{this.props.data.email}</small></label>
+      </li>
+    )
+  }
+}
+// Page Footer
 class Footer extends React.Component {
   render() {
       return (
@@ -106,5 +180,4 @@ class Footer extends React.Component {
     );
   }
 }
-
 
