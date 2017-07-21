9741c10cebb620257b586106f639eae657685dab
Victor Idongesit <victoridongesit@gmail.com>
2017-07-21 13:07:00 +0100

    remove stray comment

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,12 +2,6 @@ import React, { Component } from 'react';
 import Main from './Main.jsx';
 import Header from './Header.jsx';
 
-// const App = () => {
-//   <div>
-//     <Main/>
-//   </div>
-// }
-
 class App extends React.Component {
   render() {
     return(
