2c10a24d69821ecc2db704a8ad73c330b5cae617
Victor Idongesit <victoridongesit@gmail.com>
2017-07-22 23:51:19 +0100

    add routes to load data in batches

--- server/routes/group.js
+++ server/routes/group.js
@@ -9,9 +9,17 @@ router.use(tokenValidator.validateToken);
 router.post('/', groupController.create);
 router.post('/:id/user', groupController.adduser);
 router.post('/:id/message', groupController.postmessage);
+
+// Get all messages (paginated)
+router.get('/:id/messages/:offset/:limit', groupController.getmessages);
+
+// Get all messages (all)
 router.get('/:id/messages', groupController.getmessages);
 
-// Get group members
+// Get group members (paginated)
+router.get('/:id/members/:offset/:limit', groupController.getmembers);
+
+// Get group members (all)
 router.get('/:id/members', groupController.getmembers);
 
 // Delete group members
