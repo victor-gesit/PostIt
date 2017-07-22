bc6185f884f2baa378dbf160f2ba7664fbe012a8
Victor Idongesit <victoridongesit@gmail.com>
2017-07-22 23:52:32 +0100

    update migration file

--- server/migrations/20170621150614-create-group.js
+++ server/migrations/20170621150614-create-group.js
@@ -19,6 +19,10 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false
       },
+      creatorEmail: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
--- server/migrations/20170621151112-create-message.js
+++ server/migrations/20170621151112-create-message.js
@@ -24,6 +24,10 @@ module.exports = {
           key: 'id'
         }
       },
+      priority: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
