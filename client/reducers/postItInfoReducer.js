d7fe30687f839673b9348a4ed117d152c2208aca
Victor Idongesit <victoridongesit@gmail.com>
2017-07-28 18:34:11 +0100

    update postInfoReducer to structure data from db

--- client/reducers/postItInfoReducer.js
+++ client/reducers/postItInfoReducer.js
@@ -1,24 +1,30 @@
-const structurePostItMembersFromDb = (state, allMembers) => {
-  const postItMembers = Object.assign({}, state);
-  for (let i = 0; i < allMembers.length; i += 1) {
-    postItMembers.members[allMembers[i].id] = allMembers[i];
+const structurePostItMembersFromDb = (state, dbSnapshot) => {
+  const appState = Object.assign({}, state);
+  const membersRows = dbSnapshot.rows;
+  for (let i = 0; i < membersRows.length; i += 1) {
+    const userId = membersRows[i].id;
+    appState.members.postItMembers[userId] = membersRows[i];
   }
-  return postItMembers;
+  appState.members.meta.count = dbSnapshot.count;
+  return appState;
 };
 
-const structurePostitGroupsFromDb = (state, allGroups) => {
-  const postItGroups = Object.assign({}, state);
-  for (let i = 0; i < allGroups.length; i += 1) {
-    postItGroups.groups[allGroups[i].id] = allGroups[i];
+const structurePostitGroupsFromDb = (state, dbSnapshot) => {
+  const appState = Object.assign({}, state);
+  const groupRows = dbSnapshot.rows;
+  for (let i = 0; i < groupRows.length; i += 1) {
+    const groupId = groupRows[i].id;
+    appState.groups.postItMembers[groupId] = groupRows[i];
   }
-  return postItGroups;
+  appState.groups.meta.count = dbSnapshot.count;
+  return appState;
 };
 
 const postItInfoReducer = (state = {}, action) => {
   const appState = Object.assign({}, state);
   switch (action.type) {
-    case 'GET_POSTIT_MEMBERS_SUCCESS':
-      return structurePostItMembersFromDb(appState, action.postItUsers);
+    case 'GET_POST_IT_MEMBERS_SUCCESS':
+      return structurePostItMembersFromDb(appState, action.dbSnapShot);
     case 'GET_ALL_GROUPS_SUCCESS':
       return structurePostitGroupsFromDb(state, action.postItGroups);
     default:
