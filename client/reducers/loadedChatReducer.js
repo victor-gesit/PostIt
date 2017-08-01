1a56730e815d875fd919ef3b64a8a61d1750b23a
Victor Idongesit <victoridongesit@gmail.com>
2017-08-01 19:32:34 +0100

    make reducer handle when a group is created and deleted

--- client/reducers/loadedChatReducer.js
+++ client/reducers/loadedChatReducer.js
@@ -4,6 +4,14 @@ const loadedChatReducer = (state = {}, action) => {
       const groupId = action.groupId;
       return Object.assign({}, state, { groupId });
     }
+    case 'CREATE_GROUP_SUCCESS': {
+      const groupId = action.data.createdGroup.id;
+      return Object.assign({}, state, { groupId });
+    }
+    case 'DELETE_GROUP_SUCCESS': {
+      const groupId = '';
+      return Object.assign({}, state, groupId);
+    }
     default:
       return state;
   }
