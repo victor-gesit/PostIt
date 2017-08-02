aaf61c0d674a846432595fa32096075285a5b33d
Victor Idongesit <victoridongesit@gmail.com>
2017-08-02 19:28:28 +0100

    make redirect object have a destination

--- client/reducers/errorReducer.js
+++ client/reducers/errorReducer.js
@@ -2,116 +2,190 @@ const errorReducer = (state = {}, action) => {
   switch (action.type) {
     case 'SIGN_IN_ERROR':
       return {
+        message: action.message,
         errored: true,
-        redirect: false,
-        message: action.message
+        redirect: {
+          yes: false,
+          to: null
+        }
+      };
+    case 'SIGN_IN_SUCCESS':
+      return {
+        message: action.message,
+        errored: false,
+        redirect: {
+          yes: false,
+          to: null
+        }
       };
     case 'SIGN_UP_ERROR':
       return {
+        message: action.message,
         errored: true,
-        redirect: false,
-        message: action.message
+        redirect: {
+          yes: false,
+          to: null
+        }
       };
     case 'ADD_MEMBER_ERROR':
       return Object.assign({}, state, {
         message: action.message,
-        redirect: false,
+        redirect: {
+          yes: false,
+          to: null
+        },
         errored: true
       });
     case 'ADD_MEMBER_SUCCESS':
       return Object.assign({}, state, {
         message: action.message,
-        redirect: true,
+        redirect: {
+          yes: false,
+          to: null
+        },
         errored: false
       });
     case 'DELETE_MEMBER_ERROR':
       return Object.assign({}, state, {
         message: action.message,
-        redirect: false,
+        redirect: {
+          yes: false,
+          to: null
+        },
         errored: true
       });
     case 'DELETE_MEMBER_SUCCESS':
       return Object.assign({}, state, {
         message: action.message,
-        redirect: false,
+        redirect: {
+          yes: false,
+          to: null
+        },
         errored: false
       });
     case 'DELETE_A_GROUP_SUCCESS':
       return Object.assign({}, state, {
         message: action.message,
-        redirect: true,
+        redirect: {
+          yes: true,
+          to: '/messageboard'
+        },
         errored: false
       });
     case 'GET_GROUP_MEMBERS_ERROR':
       return Object.assign({}, state, {
         message: action.message,
-        redirect: false,
+        redirect: {
+          yes: false,
+          to: null
+        },
         errored: true
       });
     case 'GET_GROUP_MEMBERS_SUCCESS':
       return Object.assign({}, state, {
         message: action.message,
-        redirect: false,
+        redirect: {
+          yes: false,
+          to: null
+        },
         errored: false
       });
     case 'GET_ALL_GROUPS_ERROR':
       return Object.assign({}, state, {
         message: action.message,
-        redirect: false,
+        redirect: {
+          yes: false,
+          to: null
+        },
         errored: true
       });
     case 'GET_ALL_GROUPS_SUCCESS':
       return Object.assign({}, state, {
         message: action.message,
-        redirect: false,
+        redirect: {
+          yes: false,
+          to: null
+        },
         errored: false
       });
     case 'POST_MESSAGE_ERROR':
       return Object.assign({}, state, {
         message: action.message,
-        redirect: false,
+        redirect: {
+          yes: false,
+          to: null
+        },
         errored: true
       });
     case 'POST_MESSAGE_SUCCESS':
       return Object.assign({}, state, {
         message: action.message,
-        redirect: false,
+        redirect: {
+          yes: false,
+          to: null
+        },
         errored: false
       });
     case 'GET_MESSAGES_ERROR':
       return Object.assign({}, state, {
         message: action.message,
-        redirect: false,
+        redirect: {
+          yes: false,
+          to: null
+        },
         errored: true
       });
     case 'CREATE_GROUP_ERROR':
       return Object.assign({}, state, {
         message: action.message,
-        redirect: false,
+        redirect: {
+          yes: false,
+          to: null
+        },
         errored: true
       });
     case 'CREATE_GROUP_SUCCESS':
       return Object.assign({}, state, {
         message: action.message,
-        redirect: true,
+        redirect: {
+          yes: true,
+          to: '/postmessage'
+        },
         errored: false
       });
     case 'GET_MESSAGES_SUCCESS':
       return Object.assign({}, state, {
         message: action.message,
-        redirect: false,
+        redirect: {
+          yes: false,
+          to: null
+        },
         errored: false
       });
     case 'RESET_ERROR_LOG':
       return Object.assign({}, state, {
         message: null,
-        redirect: false,
+        redirect: {
+          yes: false,
+          to: null
+        },
         errored: false
+      });
+    case 'LOAD_MESSAGES':
+      return Object.assign({}, state, {
+        message: null,
+        redirect: {
+          yes: true,
+          to: '/postmessage'
+        }
       });
     case 'RESET_REDIRECT_STATE':
       return Object.assign({}, state, {
         message: null,
-        redirect: false,
+        redirect: {
+          yes: false,
+          to: null
+        },
         errored: false
       });
     default:
