4d48fe3f70fe981318f7abdbc4c1c8d64206b567
Victor Idongesit <victoridongesit@gmail.com>
2017-08-13 08:11:51 +0100

    fix eslint errors

--- client/components/views/Index.jsx
+++ client/components/views/Index.jsx
@@ -2,40 +2,44 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { signIn, resetErrorLog, resetRedirect, verifyToken } from '../../actions';
 import NotificationSystem from 'react-notification-system';
+import SignInForm from './partials/SignInForm.jsx';
+import Footer from './partials/Footer.jsx';
 
 class Index extends React.Component {
-  constructor(props){
-    super(props);
-  };
-  componentWillMount() {
-    const token = localStorage.getItem('token');
-    if(token !== 'undefined' && token !== 'null' && token !== null) {
-      this.props.verifyToken(token);
-    }
-  }
+  /**
+   * Render method of React component
+   * @returns {Object} Returns the DOM object to be rendered
+   */
   render() {
     const redirect = this.props.apiError.redirect;
-    if(redirect.yes) {
+    if (redirect.yes) {
       this.props.resetRedirect();
       // window.location = redirect.to;
     }
-    return(
+    return (
       <div>
-        <Body _that={this}/>
+        <Body store={this.props}/>
       </div>
     );
   }
 }
 
-
+/**
+ * React componet that displays Navigation Bar
+ */
 class NavBar extends React.Component {
+  /**
+   * Render method of React component
+   * @returns {Object} Returns the DOM object to be rendered
+   */
   render() {
-    return(
+    return (
       <div className="navbar-fixed">
         <nav className="pink darken-4" role="navigation">
           <div className="nav-wrapper">
             <a href="#" id="brand" className="brand-logo">PostIt</a>
-            <a href="#" data-activates="mobile-demo" className="button-collapse"><i className="material-icons">menu</i></a>
+            <a href="#" data-activates="mobile-demo"
+              className="button-collapse"><i className="material-icons">menu</i></a>
             <ul className="right hide-on-med-and-down">
               <li><a href="#">About PostIt</a></li>
             </ul>
@@ -47,7 +51,8 @@ class NavBar extends React.Component {
                   </div>
                 </div>
               </li>
-              <li><a href="#"><i className="large material-icons black-text">info</i>About PostIt</a></li>
+              <li><a href="#"><i className="large material-icons black-text">info</i>
+                About PostIt</a></li>
             </ul>
           </div>
         </nav>
@@ -56,9 +61,17 @@ class NavBar extends React.Component {
   }
 }
 
+
+/**
+ * React component for displaying page body
+ */
 class Body extends React.Component {
+  /**
+   * Render method of React component
+   * @returns {Object} Returns the DOM object to be rendered
+   */
   render() {
-    return(
+    return (
       <div id="body">
         <NavBar/>
         <div id="main">
@@ -71,7 +84,8 @@ class Body extends React.Component {
           <div className="transparent-body">
             <div className="row">
               <div className="col s12 m6 l7 center">
-                <h3 className="brown-text accent-4 lighten-3 center">Why meet when you can PostIt?</h3>
+                <h3 className="brown-text accent-4\
+                  lighten-3 center">Why meet when you can PostIt?</h3>
                 <div className="row">
                   <div className="col s12 m12 l6">
                     <i className="large green-text text-darken-4 material-icons">people</i>
@@ -91,7 +105,7 @@ class Body extends React.Component {
                   </div>
                 </div>
               </div>
-              <SignInForm _that={this.props._that}/>
+              <SignInForm store={this.props.store}/>
             </div>
           </div>
 
@@ -102,134 +116,24 @@ class Body extends React.Component {
   }
 }
 
-class SignInForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.signIn = this.signIn.bind(this);
-    this.showNotification = this.showNotification.bind(this);
-    this._notificationSystem = null;
-  }
-  componentDidMount() {
-    // Initialize notification component
-    this._notificationSystem = this.notificationRef;
-    // Set focus to Sign in button
-    $('.signin-form').keypress((event) => {
-      if ((event.which && event.which == 13) || (event.keyCode && event.keyCode == 13)) {
-          $('#signInButton').click();
-          return false;
-      } else {
-          return true;
-      }
-    })
-  }
-  componentWillUpdate() {
-    this.button.focus();
-    const isSignedIn = this.props._that.props.appInfo.authState.signedIn;
-    const errorMessage = this.props._that.props.apiError.message;
-    if(isSignedIn) {
-      const token = this.props._that.props.appInfo.userDetails.token;
-      const userId = this.props._that.props.appInfo.userDetails.id;
-      const userDetails = this.props._that.props.appInfo.userDetails;
-      localStorage.setItem('userId', userId);
-      localStorage.setItem('token', token);
-      localStorage.setItem('userDetails', JSON.stringify(userDetails));
-      window.location = '/messageboard';
-    } else {
-      if(errorMessage) {
-        this.showNotification('error', errorMessage);
-        this.props._that.props.resetErrorLog();
-      }
-    }
-  }
-  signIn(e) {
-    const email = this.email.value;
-    const password = this.password.value;
-    this.props._that.props.signIn(email, password);
-  }
-  showNotification(level, message) {
-      this._notificationSystem.addNotification({
-      message: message,
-      level: level
-    });
-  }
-  render() {
-    // Style for notification
-    const style = {
-      NotificationItem: { 
-        DefaultStyle: { 
-          margin: '100px 5px 2px 1px',
-          position: 'fixed',
-          width: '320px'
-        },
-    
-        success: { 
-          color: 'red'
-        }
-      }
-    }
-    return(
-      <div id="signinform" className="col s12 m6 l5">
-        <div className="signin-form">
-          <div className="row">
-            <NotificationSystem className='notification' style={style} ref={(notificationRef) => { this.notificationRef = notificationRef }} />
-            <div>
-              <h3 className="center">Sign In</h3>
-            </div>
-            <div className="input-field col s12">
-              <input id="email" ref={(email) => { this.email = email; }} type="email" className="validate" ></input>
-              <label htmlFor="email" data-error="Enter valid email">Email</label>
-            </div>
-            <div className="input-field col s12">
-              <input id="password" ref={(password) => { this.password = password; }}  type="password" className="validate" />
-              <label htmlFor="password">Password</label>
-            </div>
-            <div className="col s12 center">
-              <button id="signInButton" onClick={this.signIn} className="btn green darken-4" ref={(button) => { this.button = button; }} >Sign in</button>
-            </div>
-            <br /><br />
-            <div className="col s12">
-              <input id="signedin" className="teal-text" type="checkbox" name="signedin" />
-              <label htmlFor="signedin">Keep me signed in</label>
-            </div>
-            <div>
-              <p>Don't have an account? <a href="/signup">Sign up</a></p>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
-}
-
-class Footer extends React.Component {
-  render() {
-      return (
-      <footer className="page-footer pink darken-4">
-        <div className="shift-left white-text">Built by Victor Idongesit</div>
-        <div className="footer-copyright shift-left">    © Andela, 2017</div>
-      </footer>
-    );
-  }
-}
-
-
-const mapStateToProps = (state)  => {
-  return {
+const mapStateToProps = state =>
+  ({
     apiError: state.apiError,
     dataLoading: state.dataLoading,
     appInfo: {
       userDetails: state.appInfo.userDetails,
       authState: state.appInfo.authState
     }
-  };
-}
+  });
 
-const mapDispatchToProps = (dispatch) => {
-  return {
+
+const mapDispatchToProps = dispatch =>
+  ({
     signIn: (email, password) => dispatch(signIn(email, password)),
     resetErrorLog: () => dispatch(resetErrorLog()),
     resetRedirect: () => dispatch(resetRedirect()),
-    verifyToken: (token) => dispatch(verifyToken(token))
-  };
-};
+    verifyToken: token => dispatch(verifyToken(token))
+  });
+
+
 export default connect(mapStateToProps, mapDispatchToProps)(Index);
