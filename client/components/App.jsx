8ce82fe9d08c28fe0dd8a3750bdb82e956c80e74
Victor Idongesit <victoridongesit@gmail.com>
2017-08-13 08:10:23 +0100

    fix eslint errors

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import Main from './Main.jsx';
 
+/**
+ * Parent component for the routes
+ */
 class App extends React.Component {
+  /**
+   * Render method of React component
+   * @returns {Object} Returns the DOM object to be rendered
+   */
   render() {
-    return(
+    return (
       <div>
         <Main/>
       </div>
-    )
+    );
   }
 }
 export default App;
