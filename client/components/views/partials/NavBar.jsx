415cb0a4d556c335e007b46d7b63c11e007b2dc5
Victor Idongesit <victoridongesit@gmail.com>
2017-08-17 16:39:29 +0100

    remove redundant code

--- client/components/views/partials/NavBar.jsx
+++ client/components/views/partials/NavBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import jwtDecode from 'jwt-decode';
 import Groups from './Groups.jsx';
 import GroupDeleteModal from './GroupDeleteModal.jsx';
 
@@ -12,12 +13,13 @@ export default class NavBar extends React.Component {
    */
   render() {
     const groupId = localStorage.getItem('groupId');
-    const userDetailsString = localStorage.getItem('userDetails');
-    const userDetails = JSON.parse(userDetailsString);
+    const token = localStorage.getItem('token');
+    const decode = jwtDecode(token);
+    const userDetails = decode;
     const allUserGroups = this.props.allUserGroups;
-    const path = window.location.pathname;
+    const path = this.props.store.match.path;
     let sideNavClass = 'side-nav';
-    if (path === '/postmessage') {
+    if (path === '/postmessage/:groupId') {
       sideNavClass = 'side-nav fixed';
     }
     return (
@@ -25,9 +27,9 @@ export default class NavBar extends React.Component {
         <nav className="pink darken-4">
           <div className="nav-wrapper">
             <a id="brand" className="brand-logo left">PostIt</a>
-            <a href="#" data-activates="mobile-demo" data-hover="true"
+            <a data-activates="mobile-demo" data-hover="true"
               className="button-collapse show-on-large">
-              <i className="material-icons">menu</i></a>
+              <i id="sideNavIcon" className="material-icons">menu</i></a>
             <ul className="right">
               {/* Link to messageboard shouldn't be visible on messageboard page */}
               {
@@ -36,91 +38,22 @@ export default class NavBar extends React.Component {
               </li>
                 ) : (
                 <li>
-                      <a href="/messageboard">
+                      <a href="/#/messageboard">
                         <i className="material-icons">view_module</i>
                       </a>
                 </li>
                 )
               }
               <li>
-                {/* Dropdown Trigger */}
-                <ul>
-                  <li>
-                    <a className="dropdown-button" data-alignment="right"
-                      data-constrainwidth="false" data-beloworigin="true"
-                      data-hover="true" href="#" data-activates="dropdown0">
-                      <i className="material-icons">notifications_active</i>
-                    </a>
-                  </li>
-                </ul>
-                {/* Dropdown Structure */}
-                <ul id="dropdown0" className="dropdown-content">
-                  <li><a href="#!" className="black-text">Family and Friends
-                    <span className="badge">1</span></a></li>
-                  <li><a href="#!" className="black-text">DisruptI.T. Project
-                    <span className="new pink badge">1</span></a></li>
-                </ul>
-              </li>
-              <li>
-                {/* Dropdown Trigger */}
-                <ul>
-                  <li>
-                    <a className="dropdown-button" data-alignment="right"
-                      data-constrainwidth="false" data-beloworigin="true"
-                      data-hover="true" href="#" data-activates="dropdown1">
+                    <a href="/#/creategroup">
                       <i className="material-icons">library_add</i>
                     </a>
-                  </li>
-                </ul>
-                {/* Dropdown Structure */}
-                <ul id="dropdown1" className="dropdowns dropdown-content">
-                  {
-                    path === '/creategroup' ? (
-                      <li><a className="black-text"><i className="material-icons green-text">
-                        library_add</i>Create Group</a></li>
-                    ) : (
-                      <li><a href="/creategroup" className="black-text">
-                      <i className="material-icons green-text">library_add</i>Create Group</a></li>
-                    )
-                  }
-                </ul>
-              </li>
-              <li>
-                {/* Dropdown Trigger */}
-                <ul>
-                  <li>
-                    <a className="dropdown-button" data-beloworigin="true"
-                    data-hover="true" href="#" data-activates="dropdown3">
-                      <i className="material-icons">person</i>
-                    </a>
-                  </li>
-                </ul>
-                {/* Dropdown Structure */}
-                <ul id="dropdown3" className="dropdowns dropdown-content">
-                  <li className="user-profile-container">
-                    <ul className="collection">
-                      <li className="collection-item avatar black-text">
-                        <i className="material-icons purple circle">person</i>
-                        <div className="title black-text">
-                          {userDetails.firstName} {userDetails.lastName}</div>
-                        <p>{userDetails.email}<br />{userDetails.phone}</p>
-                      </li>
-                    </ul>
-                  </li>
-                  <li>
-                    <div className="row valign-wrapper">
-                      <div className="col s12 center">
-                        <button className="btn sign-out-button black">Sign out</button>
-                      </div>
-                    </div>
-                  </li>
-                </ul>
               </li>
             </ul>
             {/* Side Nav */}
             <ul id="mobile-demo" className={sideNavClass}>
               {
-                path === '/postmessage' ? (
+                path === '/postmessage/:groupId' ? (
                 <li>
                   <div className="user-details">
                     <div className="background">
@@ -159,16 +92,16 @@ export default class NavBar extends React.Component {
                 )
               }
               {
-                path === '/postmessage' ? (
+                path === '/postmessage/:groupId' ? (
                   <li><a href="#groupDeleteModal" id={groupId}>
                     <i className="large material-icons red-text">
                     texture</i>Delete Group</a></li>
                 ) : (
-                  path === '/creategroup' ? (
+                  path === '/#/creategroup' ? (
                     <li><a><i className="large material-icons green-text">library_add</i>
                     Create New Group</a></li>
                   ) : (
-                    <li><a href='/creategroup'>
+                    <li><a href='/#/creategroup'>
                     <i className="large material-icons green-text">library_add</i>
                     Create New Group</a></li>
                   )
@@ -187,9 +120,9 @@ export default class NavBar extends React.Component {
               {/* Groups a user belongs to */}
               <Groups store={this.props.store} allUserGroups={allUserGroups}/>
               <hr />
-              <li><a href="#"><i className="large material-icons black-text">info</i>
+              <li><a><i className="large material-icons black-text">info</i>
                 About PostIt</a></li>
-              <li><a href="#"><i className="large material-icons red-text">exit_to_app</i>
+              <li><a><i className="large material-icons red-text">exit_to_app</i>
                 Sign Out</a></li>
             </ul>
           </div>
