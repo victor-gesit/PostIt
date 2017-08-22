daba8c21a4183dd587f6ce8f39adc5db5ed513c4
Victor Idongesit <victoridongesit@gmail.com>
2017-08-22 19:12:30 +0100

    reset user state after sign out

--- client/reducers/userReducer.js
+++ client/reducers/userReducer.js
@@ -1,10 +1,22 @@
+/* eslint-env browser */
 const userReducer = (state = {}, action) => {
   const userDetails = action.userDetails;
   switch (action.type) {
     case 'SIGN_IN_SUCCESS':
+      localStorage.setItem('token', userDetails.token);
       return Object.assign({}, userDetails);
     case 'SIGN_UP_SUCCESS':
+      localStorage.setItem('token', userDetails.token);
       return Object.assign({}, userDetails);
+    case 'SIGN_OUT':
+      return {
+        firstName: null,
+        lastName: null,
+        id: null,
+        token: null,
+        email: null,
+        phone: null
+      };
     default:
       return state;
   }
