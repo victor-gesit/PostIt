6135613a3ae14d0872e867980a9a407d5f0a9a3e
Victor Idongesit <victoridongesit@gmail.com>
2017-08-22 18:56:14 +0100

    remove item from local storage

--- client/components/views/partials/Groups.jsx
+++ client/components/views/partials/Groups.jsx
@@ -1,5 +1,6 @@
 /* eslint-env browser */
 import React from 'react';
+import { Link } from 'react-router-dom';
 import jwtDecode from 'jwt-decode';
 
 /**
@@ -37,41 +38,27 @@ class UserGroup extends React.Component {
     this.loadMessagesAndMembers = this.loadMessagesAndMembers.bind(this);
   }
   /**
-   * method called when component properties are changed
-   * @returns {undefined} This method returns nothing
-   */
-  componentDidUpdate() {
-    const redirect = this.props.store.apiError.redirect;
-    const path = this.props.store.match.path;
-    // Check to see what page is loading the group. /postmessage route shouldn't reload page
-    if (path !== '/postmessage' && redirect.yes) {
-      // If page is redirecting to postmessage page
-      if (redirect.to.indexOf('postmessage') !== -1) {
-        const groupId = this.props.store.appInfo.loadedMessages.groupId;
-        localStorage.setItem('groupId', groupId); // Save id of group to local storage
-      }
-      this.props.store.resetRedirect();
-      window.location = redirect.to;
-    }
-  }
-  /**
    * @param {Object} event fired when the link to load details of a group is clicked
    * @returns {undefined} This method returns nothing
    */
   loadMessagesAndMembers(event) {
     const groupId = event.target.id;
     const token = localStorage.getItem('token');
-    const decode = jwtDecode(token);
+    let decode;
+    try {
+      decode = jwtDecode(token);
+    } catch (err) {
+      this.props.store.history.push('/');
+    }
     const userId = decode.id;
-    // Load messages into the conversation page
-    this.props.store.loadMessages(groupId);
     this.props.store.getMessages(groupId, token);
 
     // Load user groups
     this.props.store.getAllGroupsForUser(userId, token);
     // Load all members of the group
     this.props.store.getGroupMembers(groupId, token);
-    localStorage.setItem('groupId', groupId);
+    // Load messages into the conversation page
+    this.props.store.loadMessages(groupId);
   }
   /**
    * Render method of React component
@@ -80,8 +67,8 @@ class UserGroup extends React.Component {
   render() {
     const groupDetails = this.props.groupDetails;
     return (
-     <li><a onClick={this.loadMessagesAndMembers} id={groupDetails.id} >
-         <i className="material-icons teal-text">people_outline</i>{groupDetails.title}</a></li>
+     <li><Link onClick={this.loadMessagesAndMembers} to={`/postmessage/${groupDetails.id}`} id={groupDetails.id} >
+         <i className="material-icons teal-text">people_outline</i>{groupDetails.title}</Link></li>
     );
   }
 }
