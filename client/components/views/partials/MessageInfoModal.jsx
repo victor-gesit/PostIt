8486925bf30aa185e62a919f8f65e741e70b66a2
Victor Idongesit <victoridongesit@gmail.com>
2017-08-24 16:25:06 +0100

    show those who have read message

---
+++ client/components/views/partials/MessageInfoModal.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+
+export default class MessageInfoModal extends React.Component {
+  /**
+   * Render method of React component
+   * @returns {Object} Returns the DOM object to be rendered
+   */
+  render() {
+    const readBy = [
+      {
+        name: 'Victor Idongesit',
+        email: 'victor.idongesit@andela.com'
+      },
+      {
+        name: 'John Doe',
+        email: 'john.doe@andela.com'
+      },
+      {
+        name: 'Jane Doe',
+        email: 'jane.doe@andela.com'
+      },
+      {
+        name: 'Johnny Doe',
+        email: 'johnny.doe@andela.com'
+      }
+    ];
+    return (
+      <div id="messageInfoModal" className="modal grey">
+        <div className="modal-content">
+          <h5 className="orange-text">Read By</h5>
+          <ul className="collection">
+            {
+              readBy.map((user, index) => {
+                return (
+                  <li key={index} className="collection-item">
+                    <div>
+                    {user.name}<br/> <small className="red-text"> { user.email}</small>
+                    <a className="secondary-content"><i className="material-icons">done_all</i></a>
+                    </div>
+                  </li>
+                );
+              })
+            }
+          </ul>
+        </div>
+        <div className="modal-footer">
+          <button className="modal-action modal-close waves-effect waves-green btn-flat green-text">
+            Close</button>
+        </div>
+      </div>
+    );
+  }
+}
