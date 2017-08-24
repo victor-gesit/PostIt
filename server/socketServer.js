4e5ff7c6ffdb4e24323c8cff183b47eb91ddeb82
Victor Idongesit <victoridongesit@gmail.com>
2017-08-24 16:26:47 +0100

    add socket io server

---
+++ server/socketServer.js
@@ -0,0 +1,26 @@
+
+import io from 'socket.io';
+ 
+export default (server, app) => {
+  const socketServer = io(server);
+  socketServer.on('connection', (client) => {
+    console.log('a user connected');
+    client.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+
+    client.on('open group', (data) => {
+      const groupId = data.groupId;
+      client.join(groupId, () => {
+        console.log(client.id, ' now in rooms ', client.rooms);
+      });
+    });
+
+    client.on('close group', (data) => {
+      const groupId = data.groupId;
+      client.leave(groupId);
+    });
+    // Make the socket available to be called from the routes
+    app.client = client;
+  });
+};
