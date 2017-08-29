d4cab590ce89ca5747a7d56879afbcbbab60ff96
Victor Idongesit <victoridongesit@gmail.com>
2017-08-29 12:54:35 +0100

    add route for google signin

--- server/routes/user.js
+++ server/routes/user.js
@@ -12,7 +12,6 @@ const jwtSecret = process.env.JWT_SECRET;
 
 const router = express.Router();
 router.use(passport.initialize());
-router.use(passport.session());
 
 // Authentication Routes
 router.post('/signin', (req, res, next) => {
@@ -72,6 +71,33 @@ router.post('/signup', (req, res, next) => {
     // Handle server errors
     res.status(500).send({ success: false, message: 'Internal Server Error' })
 );
+
+router.post('/google/login', (req, res, next) => {
+  passport.authenticate('google.custom', (err, user, info) => {
+    if (err) {
+      return res.status(401).send({
+        success: false,
+        message: 'That email address is associated with an account. Please sign in'
+      });
+    }
+    if (!user) {
+      return res.status(401).send({ success: false, message: info.message });
+    }
+    if (user) {
+      const newUser = {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        phone: user.phone,
+        id: user.id
+      };
+      const token = jwt.sign(newUser, jwtSecret, {
+        expiresIn: '2 days' // expires in 48 hours
+      });
+      return res.status(202).send({ success: true, user: newUser, token, message: 'Successful Sign In' });
+    }
+  })(req, res, next);
+});
 
 // Validate token before accessing this route
 router.use(tokenValidator.validateToken);
