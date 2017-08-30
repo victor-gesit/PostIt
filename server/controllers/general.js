2ff80624077234a70db86b034966f7a73cb990a4
Victor Idongesit <victoridongesit@gmail.com>
2017-08-30 08:34:21 +0100

    add controller for password recovery

--- server/controllers/general.js
+++ server/controllers/general.js
@@ -1,4 +1,10 @@
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
 import models from '../models';
+
+dotenv.config();
+const jwtSecret = process.env.JWT_SECRET;
 
 const User = models.User;
 const Group = models.Group;
@@ -25,5 +31,84 @@ export default {
         res.status(200).send(allGroups);
       }).catch(() =>
         res.status(401).send({ success: false, message: 'Invalid query in url' }));
+  },
+  receiveEmail: (req, res) => {
+    const email = req.body.email || '';
+    User.find({ where: { email } }).then((foundUser) => {
+      if (foundUser) {
+        const user = {
+          email: foundUser.email
+        };
+        const token = jwt.sign(user, jwtSecret, {
+          expiresIn: 600 // expires in 10 minutes
+        });
+        const mailOptions = {
+          from: process.env.EMAIL_USERNAME,
+          to: email,
+          subject: 'Password Reset',
+          html: `<p>You have requested a password reset on your PostIt account<p/><br/><b>
+          <a href="http://postit-api-victor.herokuapp.com/#/newpassword/${token}">
+          Click here</a> to reset your password
+          <br/><br/>This link is valid for 10 minutes'
+          `
+        };
+        const transporter = nodemailer.createTransport({
+          service: process.env.EMAIL_SERVICE,
+          auth: {
+            user: process.env.EMAIL_USERNAME,
+            pass: process.env.EMAIL_PASSWORD
+          }
+        });
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            console.log(error);
+            return res.status(500).send({
+              success: false,
+              message: 'Could not send email. Check your internet connection'
+            });
+          } else {
+            console.log(`Email sent: ${info.response}`);
+            return res.status(202).send({ success: true, message: 'A password reset link has been sent to your email' });
+          }
+        });
+      } else {
+        return res.status(404).send({ success: false, message: 'Email not associated with any account' });
+      }
+    }).catch(() => {
+      return res.status(401).send({ success: false, message: 'Please specify an email' });
+    });
+  },
+  resetPassword: (req, res) => {
+    const passwordToken = req.body.token || req.query.token || req.headers['x-access-token'];
+    const newPassword = req.body.newPassword;
+    if (!newPassword) {
+      return res.status(401).send({ success: false, message: 'Specify a new password' });
+    }
+    if (passwordToken) {
+      jwt.verify(passwordToken, jwtSecret, (err, decoded) => {
+        if (err) {
+          return res.status(401).send({ message: 'Expired or invalid token', error: err });
+        }
+        const email = decoded.email;
+        User.find({ where: { email } }).then((user) => {
+          user.password = newPassword;
+          user.save().then((updatedUser) => {
+            const userDetails = {
+              firstName: updatedUser.firstName,
+              lastName: updatedUser.lastName,
+              email: updatedUser.email,
+              phone: updatedUser.phone,
+              id: user.id
+            };
+            const token = jwt.sign(userDetails, jwtSecret, {
+              expiresIn: '2 days' // expires in 48 hours
+            });
+            return res.status(202).send({ success: true, user: userDetails, token, message: 'Password changed successfully' });
+          });
+        });
+      });
+    } else {
+      return res.status(401).send({ message: 'No token provided', success: false });
+    }
   }
 };
